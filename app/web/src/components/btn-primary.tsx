4be2507bce822210dff4fc80a182ef05c13d74ae
Arif <arifsp8420@gmail.com>
2021-09-27 02:06:07 +0800

    color text and button (onclick href)

--- app/web/src/components/btn-primary.tsx
+++ app/web/src/components/btn-primary.tsx
@@ -3,6 +3,6 @@ import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
 export default ({ children, onClick, type }) => {
-  const _component = useComponent("btn-primary","/app/web/src/components/btn-primary",{ onClick, type });
+  const _component = useComponent("btn-primary", "/app/web/src/components/btn-primary", { onClick, type });
   return eval(_component.render)
 }
--- app/web/src/components/mini-btn.tsx
+++ app/web/src/components/mini-btn.tsx
@@ -3,6 +3,6 @@ import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
 export default ({ children, onClick }) => {
-  const _component = useComponent("mini-btn","/app/web/src/components/mini-btn",{ onClick });
+  const _component = useComponent("mini-btn", "/app/web/src/components/mini-btn", { onClick });
   return eval(_component.render)
 }
