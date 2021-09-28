cc6a5e8fd22908bc2972050de0c302e8ccacdc9c
Arif <arifsp8420@gmail.com>
2021-09-28 22:09:24 +0800

    form, input fixed

--- app/web/src/components/input-field.tsx
+++ app/web/src/components/input-field.tsx
@@ -2,7 +2,7 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-export default ({ children, placeholder }) => {
-  const _component = useComponent("input-field","/app/web/src/components/input-field",{ placeholder });
+export default ({ children, placeholder, type, style }) => {
+  const _component = useComponent("input-field","/app/web/src/components/input-field",{ placeholder, type, style });
   return eval(_component.render)
 }
