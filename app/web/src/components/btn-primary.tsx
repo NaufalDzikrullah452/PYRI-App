fdcc88b6915e940f705ac12b52b6b9dfce3cce19
Arif <arifsp8420@gmail.com>
2021-09-28 22:09:24 +0800

    form, input fixed

--- app/web/src/components/btn-primary.tsx
+++ app/web/src/components/btn-primary.tsx
@@ -2,7 +2,7 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-export default ({ children, onClick }) => {
-  const _component = useComponent("btn-primary","/app/web/src/components/btn-primary",{ onClick });
+export default ({ children, onClick, type }) => {
+  const _component = useComponent("btn-primary","/app/web/src/components/btn-primary",{ onClick, type });
   return eval(_component.render)
 }
