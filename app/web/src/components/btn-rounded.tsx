01c78e2113d0e3f8ecd8934ea35af9504bee36f0
NaufalDzikrullah452 <naufal.dzikrullah452@gmail.com>
2021-09-28 22:51:19 +0700

    fixed margin bottom interest page

--- app/web/src/components/btn-rounded.tsx
+++ app/web/src/components/btn-rounded.tsx
@@ -3,6 +3,6 @@ import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
 export default ({ children }) => {
-const _component = useComponent("btn-rounded","/app/web/src/components/btn-rounded",{});
-return eval(_component.render);
+  const _component = useComponent("btn-rounded","/app/web/src/components/btn-rounded",{});
+  return eval(_component.render)
 }
