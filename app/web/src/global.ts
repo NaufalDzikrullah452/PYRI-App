af79a4e94b6c508919bfe598d8b0edb49e810bfd
NaufalDzikrullah452 <naufal.dzikrullah452@gmail.com>
2021-10-04 23:46:55 +0700

    change date format, and added latest  prisma schema

--- app/web/src/global.ts
+++ app/web/src/global.ts
@@ -1,1 +1,21 @@
+import { format as formatFNS, parseISO } from 'date-fns'
 export const globalVar = {}
+
+export const dateFormat = (
+    value: any,
+    format?: string,
+    
+
+) => {
+    const inputFormat = format ? format : 'dd mmmm yyyy - HH:mm'
+    try{
+        if(typeof value === 'string'){
+            return formatFNS(parseISO(value), inputFormat)
+        } else {
+            return formatFNS(value, inputFormat)
+        }
+    } catch (e) {
+        return ''
+    }
+}
+
