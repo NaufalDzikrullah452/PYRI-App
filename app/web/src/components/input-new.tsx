36c82c5947f494b5394a03b3f21e0f057c652e88
Arif <arifsp8420@gmail.com>
2021-10-07 23:56:02 +0800

    minor fixing for figma design

--- app/web/src/components/input-new.tsx
+++ app/web/src/components/input-new.tsx
@@ -2,13 +2,13 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-export default ({ children, label, type, value, onChange, style,inputStyle }) => {
+export default ({ children, label, type, value, onChange, condition }) => {
   const _component = useComponent("input-new","/app/web/src/components/input-new",{
     label,
     type,
     value,
     onChange,
-    style, inputStyle
+    condition,
   });
   return eval(_component.render)
 }
