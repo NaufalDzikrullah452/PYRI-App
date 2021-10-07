8afff517f32c3db878f1a3e4607f4e1c4319c223
Arif <arifsp8420@gmail.com>
2021-10-07 23:25:23 +0800

    Input validation, get birthday value
    - check email exist for register

--- app/web/src/global.ts
+++ app/web/src/global.ts
@@ -1,5 +1,7 @@
 import { format as formatFNS, parseISO } from 'date-fns'
 const md5 = require('md5');
+var validator = require('validator');
+var passwordValidator = require('password-validator');
 
 const dateFormat = (
     value: any,
@@ -8,8 +10,8 @@ const dateFormat = (
 
 ) => {
     const inputFormat = format ? format : 'dd mmmm yyyy - HH:mm'
-    try{
-        if(typeof value === 'string'){
+    try {
+        if (typeof value === 'string') {
             return formatFNS(parseISO(value), inputFormat)
         } else {
             return formatFNS(value, inputFormat)
@@ -19,11 +21,29 @@ const dateFormat = (
     }
 }
 
-const encrypt =(value) =>{
+const encrypt = (value) => {
     return md5(value)
 }
+const validateEmail = (value) => {
+    return validator.isEmail(value);
+}
 
+
+const validatePass = (value) => {
+    // Create a schema
+    var schema = new passwordValidator();
+    schema
+        .is().min(8)                                    // Minimum length 8
+        .is().max(16)                                  // Maximum length 100
+        .has().uppercase()                              // Must have uppercase letters
+        .has().lowercase()                            // Must have at least 2 digits
+        .has().not().spaces();                           // Should not have spaces
+
+    return schema.validate(value);
+}
 export const globalVar = {
     dateFormat,
-    encrypt
+    encrypt,
+    validateEmail,
+    validatePass
 }
