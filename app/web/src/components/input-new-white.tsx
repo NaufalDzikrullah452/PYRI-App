6bb1bb9c00ffc662a45170b2aba65fed0fa7f61f
Arif <arifsp8420@gmail.com>
2021-10-15 18:22:57 +0800

    Update, Delete authors, lil add(validation admin in page)

--- app/web/src/components/input-new-white.tsx
+++ app/web/src/components/input-new-white.tsx
@@ -2,7 +2,14 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-export default ({ children, label, type, value, onChange, condition }) => {
+export default ({
+  children,
+  label,
+  type,
+  value,
+  onChange,
+  condition,
+}) => {
   const _component = useComponent("input-new-white","/app/web/src/components/input-new-white",{
     label,
     type,
