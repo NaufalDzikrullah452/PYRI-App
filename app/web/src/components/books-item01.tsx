2a0c81c54e9292e391af01500a29d8a7b41e0fd2
Arif <arifsp8420@gmail.com>
2021-10-18 15:09:04 +0800

    admin login, routing page & generate new home component
    admin login get data from database user

--- app/web/src/components/books-item01.tsx
+++ app/web/src/components/books-item01.tsx
@@ -2,7 +2,7 @@
 import { jsx } from "@emotion/react";
 import { useComponent } from "web.utils/component";
 
-export default ({ children }) => {
-const _component = useComponent("books-item01","/app/web/src/components/books-item01",{});
-return eval(_component.render);
+export default ({ children, onClick }) => {
+  const _component = useComponent("books-item01","/app/web/src/components/books-item01",{onClick});
+  return eval(_component.render);
 }
