1c74fd771d882ff92076feef20ab52ff2669c2a4
muhammadarifsp <85152766+muhammadarifsp@users.noreply.github.com>
2022-01-10 17:57:58 +0800

    disable screencapture

--- app/mobile/android/app/src/main/java/com/andromedia/base/MainActivity.java
+++ app/mobile/android/app/src/main/java/com/andromedia/base/MainActivity.java
@@ -1,5 +1,13 @@
 package com.andromedia.base;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends BridgeActivity {}
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+}
