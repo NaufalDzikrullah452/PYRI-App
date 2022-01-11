bff5f915998a3079243f57f7345c5141ef561396
NaufalDzikrullah452 <naufal.dzikrullah452@gmail.com>
2022-01-11 08:53:25 +0700

    set width bar chart authors in dashboard

--- app/web/src/components/barchart-author.tsx
+++ app/web/src/components/barchart-author.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Bar, defaults } from 'react-chartjs-2'
 
-const BarChart = () => {
+const BarChart = (x) => {
+    const idata = x.data;
     return (
         <div>
             <Bar
@@ -11,17 +12,7 @@ const BarChart = () => {
                     responsive: true,
                     maintainAspectRatio: false,
                 }}
-                data={{
-                    labels: ['Author 1', 'Author 2', 'Author 3', 'Author 3', 'Author 4', 'Author 5', 'Author 6', 'Author 7', 'Author 8', 'Author 9', 'Author 10', 'Author 11', 'Author 12'],
-                    datasets: [
-                        {
-                            label: '# of Votes',
-                            data: [120, 19, 3, 5, 2, 3, 10, 10, 20, 70, 67, 8],
-                            backgroundColor: '#00cec9',
-                            borderRadius: 0,
-                        },
-                    ],
-                }}
+                data={idata}
             />
         </div>
     )
