1d1e129e54f947bb62953e16a1f0309542aaf65b
NaufalDzikrullah452 <naufal.dzikrullah452@gmail.com>
2022-01-25 22:01:44 +0700

    add privacy screen and fix web

--- app/mobile/android/app/src/main/java/com/andromedia/base/MainActivity.java
+++ app/mobile/android/app/src/main/java/com/andromedia/base/MainActivity.java
@@ -1,13 +1,6 @@
 package com.andromedia.base;
 
 import com.getcapacitor.BridgeActivity;
-import android.os.Bundle;
-import android.view.WindowManager;
+
 
 public class MainActivity extends BridgeActivity {}
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
-}
